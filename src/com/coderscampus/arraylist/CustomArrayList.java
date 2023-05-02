ac46bca127d4c8de7fcf19ed38b85ad6be912ff9
amamolo9986 <amamolo9986@yahoo.com>
2023-05-02 12:34:50 -0500

    debugged the growth rate of array

--- src/com/coderscampus/arraylist/CustomArrayList.java
+++ src/com/coderscampus/arraylist/CustomArrayList.java
@@ -4,6 +4,7 @@ public class CustomArrayList<T> implements CustomList<T> {
 
 	private final int initialCapacity = 10;
 	private Object[] originalArray;
+	private Object[] newArray;
 	private int size;
 
 	// creating our variables
@@ -25,13 +26,11 @@ public class CustomArrayList<T> implements CustomList<T> {
 
 	@Override
 	public boolean add(T item) {
-		for (Object element : originalArray) { // loops every time our array hits capacity
-			if (size == initialCapacity) { // if the size of the array equals the initial capacity
-				Object[] newArray = new Object[originalArray.length * 2]; // create a newItems array that equals old *2
+			if (size == originalArray.length) { // if the size of the array equals the initial capacity
+				newArray = new Object[originalArray.length * 2]; // create a newItems array that equals old *2
 				System.arraycopy(originalArray, 0, newArray, 0, originalArray.length); // then copy the original array and paste to new array
 				originalArray = newArray;
 			}
-		}
 		originalArray[size] = item; // adding new items
 		size++; // incrementing array size
 		return true;
